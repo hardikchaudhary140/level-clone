ca01508d3b04c7aa3f311c9ff4e6067491e98a6d
amitvchaudhary <amit.chaudhary140@gmail.com>
2021-03-15 13:40:18 +0530

    created initial structure

--- level/src/app/app.module.ts
+++ level/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { ServiceComponent } from './service/service.component';
+import { VideoComponent } from './video/video.component';
+import { CheckInComponent } from './check-in/check-in.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContactUsComponent,
+    ServiceComponent,
+    VideoComponent,
+    CheckInComponent
   ],
   imports: [
     BrowserModule,
